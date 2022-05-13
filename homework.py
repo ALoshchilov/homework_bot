1f58da1aaabc9f8d03f16f807815c71e1360c15d
Loshchilov Aleksandr <loshchilov.aleksandr@gmail.com>
2022-05-13 23:42:05 +0300

    Working autotest, not working bot

--- homework.py
+++ homework.py
@@ -1,20 +1,20 @@
 from datetime import datetime, timedelta
 import logging
 from os import getenv
-import requests
-import time
 from sys import stdout
+import time
 
 from dotenv import load_dotenv
+import requests
 from telegram import Bot
 
-from exceptions import NetworkError, HttpResponseError
 
 load_dotenv(override=True)
-PRACTICUM_TOKEN = str(getenv('PRACTICUM_TOKEN'))
-TELEGRAM_TOKEN = str(getenv('TELEGRAM_TOKEN'))
+PRACTICUM_TOKEN = getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = getenv('TELEGRAM_CHAT_ID')
-RETRY_TIME = 600
+ENV_VARS = ['PRACTICUM_TOKEN', 'TELEGRAM_CHAT_ID', 'TELEGRAM_TOKEN']
+RETRY_TIME = 15
 SUCCESS_RESPONSE_CODE = 200
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
@@ -25,35 +25,56 @@ HOMEWORK_VERDICTS = {
 }
 LOG_PATH = __file__ + '.log'
 # Шаблоны сообщений и записей лога
-OK_SEND_MESSAGE_TEMPLATE = (
-    'Message: "{message}" successfully send '
-    'to chat ({chat_id})'
+BAD_ENV_VAR_ERROR_TEMPLATE = (
+    'Unexisting or empty environment variable ({name}) was found'
 )
 BAD_SEND_MESSAGE_TEMPLATE = (
-    'Cannot send message: "{message}" to chat ({chat_id})'
+    'Cannot send message: "{message}" to chat ({chat_id}). Error: {error}.'
 )
 CHANGED_HOMEWORK_STATUS_TEMPLATE = (
     'Изменился статус проверки работы "{homework_name}". {verdict}'
 )
-BAD_ENV_VAR_ERROR_TEMPLATE = (
-    'Unexisting or empty environment variable ({name}) was found'
+CONNECTION_ERROR_TEMPLATE = (
+    'An error occured during request {url}.'
+    'DETAILS. Headers: {headers}. Params: {params}'
+)
+OK_SEND_MESSAGE_TEMPLATE = (
+    'Message: "{message}" successfully send to chat ({chat_id})'
+)
+RESPONSE_ERROR_IN_JSON_TEMPLATE = (
+    'An error was found in json response from {url} '
+    'DETAILS: Response code got: {code} expected: {expected_code} '
+    'Headers: {headers}. Parameters: {params} '
+    'The error detected in key: {error_key}. Error description: "{error_text}"'
+)
+STOP_BOT_TEMPLATE = ' Stoping bot... Reasone: {reasone}'
+UNKNOWN_HOMEWORK_STATUS_TEMPLATE = 'Status "{homework_status}" is unknown'
+WRONG_HTTP_RESPONSE_ERROR_TEMPLATE = (
+    'Wrong HTTP response code from {url}. '
+    'Got: {code}. Expected: {expected_code}. '
+    'Details. HEADERS: {headers}, PARAMETERS: {params}'
 )
 WRONG_TYPE_MESSAGE_TEMPLATE = (
     'Wrong datatype for {object} '
     'Got: {got}. Expected: {expected}.'
 )
-UNKNOWN_HOMEWORK_STATUS_TEMPLATE = 'Status "{homework_status}" is unknown'
-STOP_BOT_MESSAGE = 'Stoping bot...'
-START_BOT_MESSAGE = 'Starting bot...'
+
 BOT_INIT_ERR_MESSAGE = 'Error durining bot initializing. Check telegram token'
+LAST_FRONTIER_ERROR = (
+    'An error ocurred during the itteration. Details is below in the log:'
+)
 NO_CHECKED_WORKS_MESSAGE = 'No new checked homeworks were found'
 NO_GET_API_ANSWER_RESPONSE = 'Empty response from get_api_answer.'
+SEND_ERROR_INFO_EXCEPTION = (
+    f'An error occured during send error info to chat ({TELEGRAM_CHAT_ID})'
+)
+START_BOT_MESSAGE = 'Starting bot...'
 
 
 def logger_init():
     """Инициализация настроек логирования..."""
     handlers = [
-        logging.StreamHandler(stdout),
+        #logging.StreamHandler(stdout),
         logging.FileHandler(LOG_PATH),
     ]
     logging.basicConfig(
@@ -78,10 +99,11 @@ def send_message(bot, message):
             message=message,
             chat_id=TELEGRAM_CHAT_ID
         ))
-    except Exception:
+    except Exception as error:
         logging.exception(BAD_SEND_MESSAGE_TEMPLATE.format(
             message=message,
-            chat_id=TELEGRAM_CHAT_ID
+            chat_id=TELEGRAM_CHAT_ID,
+            error=error
         ))
 
 
@@ -93,24 +115,36 @@ def get_api_answer(current_timestamp: int):
     JSON к типам данных Python.
     """
     params = {'from_date': current_timestamp}
-    try:
-        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
-    except Exception:
-        raise NetworkError(url=ENDPOINT, headers=HEADERS, params=params)
     response_details = {
-        'code': response.status_code,
-        'expected_code': SUCCESS_RESPONSE_CODE,
         'url': ENDPOINT,
         'headers': HEADERS,
-        'params': params,
+        'params': params
     }
+    try:
+        response = requests.get(**response_details)
+    except requests.RequestException:
+        raise requests.ConnectionError(CONNECTION_ERROR_TEMPLATE.format(
+            **response_details
+        ))
+    response_details.update({
+        'code': response.status_code,
+        'expected_code': SUCCESS_RESPONSE_CODE
+    })
     if response.status_code != SUCCESS_RESPONSE_CODE:
-        raise HttpResponseError(**response_details)
-    if isinstance(response, list):
-        response = response[0]
-    if ('error' or 'code') in response.json():
-        raise HttpResponseError(**response_details)
-    return response.json()
+        raise requests.HTTPError(WRONG_HTTP_RESPONSE_ERROR_TEMPLATE.format(
+            **response_details
+        ))
+    response_json = response.json()
+    for error_key in ['error', 'code']:
+        if error_key in response_json:
+            response_details.update({
+                'error_key': error_key,
+                'error_text': response_json.get(error_key)
+            })
+            raise requests.HTTPError(RESPONSE_ERROR_IN_JSON_TEMPLATE.format(
+                **response_details
+            ))
+    return response_json
 
 
 def check_response(response: dict):
@@ -121,17 +155,19 @@ def check_response(response: dict):
     """
     if not response:
         raise IndexError(NO_GET_API_ANSWER_RESPONSE)
+    if not isinstance(response, dict):
+        raise TypeError(WRONG_TYPE_MESSAGE_TEMPLATE.format(
+            object='check_response argument',
+            got=type(response),
+            expected='dict'
+        ))
     homeworks = response['homeworks']
     if not isinstance(homeworks, list):
         raise TypeError(WRONG_TYPE_MESSAGE_TEMPLATE.format(
             object='homeworks info',
             got=type(response),
-            expected='dict'
+            expected='list'
         ))
-    # проверка current_date
-    response['current_date']
-    if homeworks:
-        homeworks = homeworks[0]
     return homeworks
 
 
@@ -142,16 +178,11 @@ def parse_status(homework):
     отправки в Telegram строку, содержащую один из вердиктов словаря
     HOMEWORK_VERDICTS.
     """
-    if not isinstance(homework, dict):
-        raise KeyError(WRONG_TYPE_MESSAGE_TEMPLATE.format(
-            object='parse_status argument',
-            got=type(homework),
-            expected='dict'
-        ))
+    print(homework)
     homework_status = homework['status']
     homework_name = homework['homework_name']
     if homework_status not in HOMEWORK_VERDICTS:
-        raise KeyError(UNKNOWN_HOMEWORK_STATUS_TEMPLATE.format(
+        raise ValueError(UNKNOWN_HOMEWORK_STATUS_TEMPLATE.format(
             homework_status=homework_status
         ))
     return CHANGED_HOMEWORK_STATUS_TEMPLATE.format(
@@ -165,7 +196,6 @@ def check_tokens():
     программы. Если отсутствует хотя бы одна переменная окружения — функция
     должна вернуть False, иначе — True.
     """
-    ENV_VARS = ['PRACTICUM_TOKEN', 'TELEGRAM_CHAT_ID', 'TELEGRAM_TOKEN']
     env_vars_correct = True
     for name in ENV_VARS:
         value = globals().get(name)
@@ -179,17 +209,13 @@ def main():
     """Основная логика работы бота..."""
     logging.info(START_BOT_MESSAGE)
     if not check_tokens():
-        logging.info(STOP_BOT_MESSAGE)
-        exit()
-    try:
-        bot = Bot(token=TELEGRAM_TOKEN)
-    except Exception:
-        logging.critical(BOT_INIT_ERR_MESSAGE)
-        logging.info(STOP_BOT_MESSAGE)
-        exit()
+        message = STOP_BOT_TEMPLATE.format(reasone='Bad environment variables')
+        logging.info(message)
+        raise ValueError(message)
+    bot = Bot(token=TELEGRAM_TOKEN)
     last_error = None
     current_timestamp = int(
-        (datetime.today() - timedelta(seconds=RETRY_TIME)).timestamp()
+        (datetime.today() - timedelta(days=30)).timestamp()
     )
     while True:
         try:
@@ -197,14 +223,16 @@ def main():
             homework = check_response(response)
             if homework:
                 send_message(bot, parse_status(homework))
-        except Exception as error:
-            logging.exception(error)
-            if last_error != str(error):
-                send_message(bot, str(error))
-                last_error = str(error)
-        else:
-            last_error = None
             current_timestamp = response.get('current_date', current_timestamp)
+            last_error = None
+        except Exception as error:
+            logging.exception(LAST_FRONTIER_ERROR)
+            if last_error != str(error):
+                try:
+                    send_message(bot, str(error))
+                    last_error = str(error)
+                except Exception:
+                    logging.exception(SEND_ERROR_INFO_EXCEPTION)
         finally:
             time.sleep(RETRY_TIME)
 
